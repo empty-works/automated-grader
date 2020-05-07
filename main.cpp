d0ff69a01af52f8b7e560632958efb0b543e90d6
empty-works <pama.mark@gmail.com>
2020-05-07 04:35:30 +0200

    Added beginning text with appropriate alignment.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+int main() {
+
+	std::cout << std::setw(30) << std::left << "Student";
+	std::cout << "Score" << std::endl;
+	std::cout << "-----------------------------------" << std::endl;
+	
+		
+
+	return 0;
+}
